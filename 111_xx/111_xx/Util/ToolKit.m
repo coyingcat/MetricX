37be5a67fc4f3dddfe0e648df2f2dc3852d9dc70
邓江洲 <dengjiangzhouReal@gmail.com>
2022-06-22 02:37:14 +0800

    more kit

--- 111_xx/111_xx/Util/ToolKit.m
+++ 111_xx/111_xx/Util/ToolKit.m
@@ -6,7 +6,7 @@
 //
 
 #import "ToolKit.h"
-
+#import <os/proc.h>
 
 #include <mach/mach.h>
 
@@ -43,5 +43,46 @@ static ToolKit * kit = nil;
     }
 }
 
+
+- (NSInteger)totalMemoryForDevice {
+    return (NSInteger)([NSProcessInfo processInfo].physicalMemory/1024/1024);
+}
+
+
+
+- (NSInteger)availableSizeOfMemory {
+    if (@available(iOS 13.0, *)) {
+        return (NSInteger)(os_proc_available_memory() / 1024.0 / 1024.0);
+    }
+    return 0;
+}
+
+
+
+- (NSInteger)limitSizeOfMemory {
+    if (@available(iOS 13.0, *)) {
+        task_vm_info_data_t taskInfo;
+        mach_msg_type_number_t infoCount = TASK_VM_INFO_COUNT;
+        kern_return_t kernReturn = task_info(mach_task_self(), TASK_VM_INFO, (task_info_t)&taskInfo, &infoCount);
+
+        if (kernReturn != KERN_SUCCESS) {
+            return 0;
+        }
+        return (NSInteger)((taskInfo.phys_footprint + os_proc_available_memory()) / 1024.0 / 1024.0);
+    } else {
+        NSInteger totalMemory = [self totalMemoryForDevice];
+        NSInteger limitMemory;
+        if (totalMemory <= 1024) {
+            limitMemory = totalMemory * 0.45;
+        } else if (totalMemory >= 1024 && totalMemory <= 2048) {
+            limitMemory = totalMemory * 0.45;
+        } else if (totalMemory >= 2048 && totalMemory <= 3072) {
+            limitMemory = totalMemory * 0.50;
+        } else {
+            limitMemory = totalMemory * 0.55;
+        }
+        return limitMemory;
+    }
+}
 
 @end
